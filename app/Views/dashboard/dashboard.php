de6d37ec17276dcacba9b10338c6b0e0f4b3e604
Manabat, Markrey B <92512534+Marky3099@users.noreply.github.com>
2022-09-24 18:13:02 +0800

    complete out of total

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -423,14 +423,14 @@
                         </div>
 
                         <!--Completed -->
-                        
-                        
+
+
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-info shadow h-100 py-2">
                                 <div class="card-body1">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-uppercase mb-1">Completed
+                                            <div class="text-xs font-weight-bold text-uppercase mb-1">Completed: <?= json_encode($complete_event);?> out of <?= json_encode($event_total);?>
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
