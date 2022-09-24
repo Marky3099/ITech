daa0c9bd2336c8e776c6a91add14e14d66a8cb80
Manabat, Markrey B <92512534+Marky3099@users.noreply.github.com>
2022-09-24 16:51:07 +0800

    changes

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -438,16 +438,16 @@
 
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?=base_url("js/vendor/jquery/jquery.min.js")?>"></script>
-    <script src="<?=base_url("js/vendor/bootstrap/js/bootstrap.bundle.min.js")?>"></script>
+    <script src="<?=base_url("js/jquery.min.js")?>"></script>
+    <script src="<?=base_url("js/bootstrap.bundle.min.js")?>"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?=base_url("js/vendor/jquery-easing/jquery.easing.min.js")?>"></script>
+    <script src="<?=base_url("js/jquery.easing.min.js")?>"></script>
     <!-- Custom scripts for all pages-->
     <script src="<?=base_url("js/sb-admin-2.min.js")?>"></script>
 
     <!-- Page level plugins -->
-    <script src="<?=base_url("js/vendor/chart.js/Chart.min.js")?>"></script>
+    <script src="<?=base_url("js/Chart.min.js")?>"></script>
 
     <!-- Page level custom scripts -->
     <script src="<?=base_url("js/chart-area-demo.js")?>"></script>
