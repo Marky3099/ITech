d976b2d8059fffc3494fb1e369f0489b31ade7fd
zahssabasa <zahssabasa4@gmail.com>
2022-09-30 11:16:59 +0800

    New Aircon Forms created

--- app/Views/aircon/aircon_add.php
+++ app/Views/aircon/aircon_add.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="<?= base_url('assets/css/formstyle.css')?>">
 <div class="body-content">
   <div class="add-form">
     <form method="post" id="add_aircon" name="add_aircon" 
@@ -7,28 +8,25 @@
                     <?= $error ?>
                   </div>
               <?php }?>
-  
-    <h1>Add Aircon</h1>
-    <div class="form-content long">
-      <div class="form-group">
-        <label id="label1">Device Brand</label>
-        <input type="text" name="device_brand" class="form-control" required>
-      </div>
+    
+    <div class="form-box">
+        <h3>Add Aircon Details</h3><br>
+        <div class="user-box">
+           <div class="icon-box"><i class="fas fa-wrench"></i></div>
+          <input type="text" name="device_brand" placeholder="Device Brand" required>
+        </div>
 
-      <div class="form-group">
-        <label>Aircon Type</label>
-        <input type="text" name="aircon_type" class="form-control" required>
-      </div>
+        <div class="user-box">
+           <div class="icon-box"><i class="fas fa-wrench"></i></div>
+           <input type="text" name="aircon_type" placeholder="Aircon type" required>
+        </div><br>
 
-      <div class="form-group">
-        <button type="submit" class="btn btn-success">Add Data</button>
-      </div>
-      <div class="form-group">
-        <a href="<?= base_url('/aircon');?>" class="btn btn-secondary back">Back</a>
-      </div>
+        <div class="container1">
+          <button type="submit" class="btn btn-success">Add Data</button>
+          <a href='<?=base_url('/aircon')?>' class="back-btn">Back</a>
+        </div>
     </div>
+
     </form>
   </div>
 </div>
-</div>
- 
