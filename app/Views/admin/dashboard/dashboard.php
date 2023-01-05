1b38f12b31a4142ce12b14da86ff1a44f7672b49
zahssabasa <zahssabasa4@gmail.com>
2023-01-05 19:24:35 +0800

    minor changs
    typo errors

--- app/Views/admin/dashboard/dashboard.php
+++ app/Views/admin/dashboard/dashboard.php
@@ -310,7 +310,7 @@
             <div class="modal-content">
 
               <div class="modal-header">
-                <h4 class="modal-title">Pending Task</h4>
+                <h4 class="modal-title">Pending Requests</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 
               </div>
