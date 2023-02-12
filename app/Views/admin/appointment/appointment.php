582dd2271b1e7cb70e31080fa6dcff4f8d2ff6c5
Manabat, Markrey B <92512534+Marky3099@users.noreply.github.com>
2023-02-12 09:59:06 +0800

    view ratings on appointment

--- app/Views/admin/appointment/appointment.php
+++ app/Views/admin/appointment/appointment.php
@@ -1,6 +1,34 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
 <link rel="stylesheet" href="<?= base_url('assets/css/main.min.css')?>">
 
+
+<!-- modal for viewing rate service -->
+<div class="modal fade" id="rateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Service Review</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="container rate-container">
+          
+          
+        </div>
+        
+        
+        <h6></h6>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
@@ -115,7 +143,10 @@
            <td><?php echo $appt->appt_code; ?></td>
            <td><?php echo $appt->client_branch; ?></td>
            <td><?php echo $appt->appt_status; ?></td>
-           <td><a href="#" class="btn btn-success btn-sm viewReport">View</a></td>
+           <td><?php if($appt->appt_status == 'Done'):?>
+                <a href="#" id="<?=$appt->appt_code?>" class="btn btn-success btn-sm viewReport">View</a>
+              <?php endif;?>
+          </td>
            <td>
              <a href="#" id="<?php echo $appt->appt_id;?>" class="btn btn-info btn-sm view">View</a>
              <?php if($appt->appt_status == 'Pending'):?>
@@ -193,6 +224,132 @@
         }
       })
     })
+
+var rateModal = new bootstrap.Modal(document.getElementById('rateModal'));
+   $('.viewReport').click(function(){
+      var id = $(this).attr('id');
+
+      $.ajax({
+           method:"GET",
+           url:"<?=base_url('/appointment/view-ratings')?>",
+           data: {
+              'id': id,
+           },
+           success: function(response){
+            var rate = response.rate;
+            var emp = response.emp;
+            $('.rate-container').empty();
+            if(rate.length > 0){
+              $('.rate-container').append(`<center><h2>Service's Review</h2></center>
+          <div class="row">
+            <div class="col-lg-4">
+               <p class="servq">Service Quality</p>
+            </div>
+            <div class="rate col-lg-6">
+              <input type="radio" id="star5" name="rate" value="5" disabled/>
+              <label for="star5" title="Amazing">5 stars</label>
+              <input type="radio" id="star4" name="rate" value="4" disabled/>
+              <label for="star4" title="Good">4 stars</label>
+              <input type="radio" id="star3" name="rate" value="3" disabled/>
+              <label for="star3" title="Fair">3 stars</label>
+              <input type="radio" id="star2" name="rate" value="2" disabled/>
+              <label for="star2" title="Poor">2 stars</label>
+              <input type="radio" id="star1" name="rate" value="1" disabled/>
+              <label for="star1" title="Terrible">1 star</label>
+            </div>
+            <div class="col-lg-2 result"></div>
+          </div>
+          <textarea name="comments" id="event_comments" placeholder="Share more thoughts on our service..." rows="4" cols="50" disabled></textarea>
+          <center><h2>Technician's Review</h2></center>
+          <div class="techRate" id="techRate">
+            
+          </div>`);
+
+              for(var i =0; i <rate.length; i++){
+                var empId = rate[i].emp_id;
+                if(empId == emp[i].emp_id){
+                  $('#techRate').append(`<h5>`+emp[i].emp_name+`</h5><div class="row rowa">
+                  <div class="col-lg-5">
+                     <p class="servq">Technician Quality</p>
+                  </div>
+                  <div class="tech col-lg-6">
+                    <input type="radio" id="star5`+empId+`" name="rate_`+empId+`" value="5" disabled/>
+                    <label for="star5`+empId+`" title="Amazing">5 stars</label>
+                    <input type="radio" id="star4`+empId+`" name="rate_`+empId+`" value="4" disabled/>
+                    <label for="star4`+empId+`" title="Good">4 stars</label>
+                    <input type="radio" id="star3`+empId+`" name="rate_`+empId+`" value="3" disabled/>
+                    <label for="star3`+empId+`" title="Fair">3 stars</label>
+                    <input type="radio" id="star2`+empId+`" name="rate_`+empId+`" value="2" disabled/>
+                    <label for="star2`+empId+`" title="Poor">2 stars</label>
+                    <input type="radio" id="star1`+empId+`" name="rate_`+empId+`" value="1" disabled/>
+                    <label for="star1`+empId+`" title="Terrible">1 star</label>
+                  </div>
+                  <div class="col-lg-1 resulttech" id= a`+empId+`></div>
+                  </div>
+                  <textarea name="techComments[]" id="tech`+empId+`" placeholder="Leave a comment..." rows="4" cols="50" multiple disabled></textarea>`);
+
+                  if(rate[i].rate_emp == '100'){
+                    $('#star5'+empId).prop('checked',true);
+                    $('#a'+empId).html('Amazing');
+                  }else if(rate[i].rate_emp == '80'){
+                    $('#star4'+empId).prop('checked',true);
+                    $('#a'+empId).html('Good');
+                  }else if(rate[i].rate_emp == '60'){
+                    $('#star3'+empId).prop('checked',true);
+                    $('#a'+empId).html('Fair');
+                  }else if(rate[i].rate_emp == '40'){
+                    $('#star2'+empId).prop('checked',true);
+                    $('#a'+empId).html('Poor');
+                  }else if(rate[i].rate_emp == '20'){
+                    $('#star1'+empId).prop('checked',true);
+                    $('#a'+empId).html('Terrible');
+                  }
+                  if(rate[i].emp_comments.length > 0){
+                    // console.log('com');
+                    $('#tech'+empId).html(rate[i].emp_comments);
+                  }else{
+                    $('#tech'+empId).html('No Comments');
+                  }
+
+
+                }
+              }
+
+              if(rate[0].rate_event == '100'){
+                $('#star5').prop('checked',true);
+                $('.result').html('Amazing');
+              }else if(rate[0].rate_event == '80'){
+                $('#star4').prop('checked',true);
+                $('.result').html('Good');
+              }else if(rate[0].rate_event == '60'){
+                $('#star3').prop('checked',true);
+                $('.result').html('Fair');
+              }else if(rate[0].rate_event == '40'){
+                $('#star2').prop('checked',true);
+                $('.result').html('Poor');
+              }else if(rate[0].rate_event == '20'){
+                $('#star1').prop('checked',true);
+                $('.result').html('Terrible');
+              }
+
+              if(rate[0].event_comments.length > 0){
+                $('#event_comments').html(rate[0].event_comments);
+              }else{
+                $('#event_comments').html('No Comments');
+              }
+
+
+
+            }else{
+              $('.rate-container').append('<center><h1>No Reviews Yet</h1></center>');
+            }
+           }
+      });
+
+      rateModal.show();
+   })
+
+
 
    $(document).on('click','.view',function(e){
       // console.log(e.target.id);
