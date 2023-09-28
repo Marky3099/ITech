84820737594685ba8cc9a7b11d1460bec7201e68
Manabat, Markrey B <92512534+Marky3099@users.noreply.github.com>
2023-09-28 13:56:54 +0800

    console

--- app/Views/client/appointment/appointment_view.php
+++ app/Views/client/appointment/appointment_view.php
@@ -282,7 +282,7 @@ $(document).ready(function() {
             'appt_id': id
          },
          success: function(response){
-          // console.log(response);
+          console.log(response);
             var date = new Date(response.appt_data.appt_date);
 	console.log(date);
             var startEvent = date.toLocaleDateString("en-US",(options));
